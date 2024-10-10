b4e38d27cfb51f92c519a93e099da3f37d26380b
2NG CODER <nahomnigussie7@gmail.com>
2024-10-10 02:35:29 -0700

    all boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,26 +3,24 @@
 def canUnlockAll(boxes):
     # The number of boxes
     n = len(boxes)
-    
+
     # A set to keep track of opened boxes
-    opened = set()
-    
-    # Start by opening the first box (box 0)
-    opened.add(0)
-    
+    opened = set([0])  # Start with box 0 as it's already unlocked
+
     # Stack to manage keys, initialized with keys in box 0
     keys = boxes[0].copy()
-    
+
     # Loop while we have keys to check
     while keys:
         key = keys.pop()  # Take a key from the stack
-        
+
+        # Only unlock the box if the key corresponds to a valid box
+        # and the box hasn't been unlocked yet
         if key < n and key not in opened:
-            # Open the box if it hasn't been opened
-            opened.add(key)
-            
-            # Add all keys from the newly opened box to the stack
+            opened.add(key)  # Open the box
+
+            # Add all the keys from the newly opened box
             keys.extend(boxes[key])
-    
-    # If we opened all boxes, return True, else return False
+
+    # Check if we've unlocked all boxes
     return len(opened) == n
