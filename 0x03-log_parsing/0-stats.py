e128be4ab8bbb584a71540d044d38dfea8623e15
2NG CODER <nahomnigussie7@gmail.com>
2024-10-23 14:58:42 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
+"""Module containing script that reads stdin and computes metrics"""
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 total_size = 0
-counter = 0
+total_num = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
+        lines = line.split(" ")
+
+        if len(lines) > 4:
+            code = lines[-2]
+            size = int(lines[-1])
+
+            if code in status_codes.keys():
+                status_codes[code] += 1
+
             total_size += size
-            counter += 1
+            total_num += 1
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+        if total_num == 10:
+            total_num = 0
+            print("File size: {}".format(total_size))
 
-except Exception as err:
+            for k, v in sorted(status_codes.items()):
+                if v != 0:
+                    print("{}: {}".format(k, v))
+
+except Exception:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print("File size: {}".format(total_size))
+
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
