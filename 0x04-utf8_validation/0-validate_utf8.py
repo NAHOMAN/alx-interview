5ec7bd3adce06e1c900ed34182ce331ec70ffcc4
2NG CODER <nahomnigussie7@gmail.com>
2024-10-31 09:28:40 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+def validUTF8(data):
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
+
+    # Masks to check the most significant bits
+    mask1 = 1 << 7    # 10000000
+    mask2 = 1 << 6    # 01000000
+
+    for byte in data:
+        # Get only the 8 least significant bits
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            if (byte >> 5) == 0b110:
+                num_bytes = 1  # 2-byte character
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2  # 3-byte character
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3  # 4-byte character
+            elif (byte >> 7) == 0:  # 1-byte character
+                continue
+            else:
+                return False
+        else:
+            # Continuation bytes must start with '10xxxxxx'
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
+
