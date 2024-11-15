f736b15a5aa28d05db4f9fdfbd606d2046d2bb97
2NG CODER <nahomnigussie7@gmail.com>
2024-11-15 09:16:56 +0000

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,44 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+const movieId = process.argv[2]; // Get the Movie ID from the command-line argument
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
+
+// URL for the Star Wars API films endpoint
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+// Make a request to get the movie details
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: Unable to fetch movie with ID ${movieId}`);
+    return;
+  }
+
+  // Parse the response body
+  const movie = JSON.parse(body);
+
+  // Get the list of character URLs
+  const characters = movie.characters;
+
+  // Fetch and print each character's name
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        return;
+      }
+
+      const character = JSON.parse(charBody);
+      console.log(character.name);
+    });
+  });
+});
